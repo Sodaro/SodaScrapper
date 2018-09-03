e286ee45d44fdceb672d7be0ca89e6bde289fde8
Sodaro <37345736+Sodaro@users.noreply.github.com>
2018-09-03 04:36:04 +0200

    Add files via upload
    /scrap now only works when the scrapper window is open
    Automatically adds items to scrapper when the window is opened

--- main.lua
+++ main.lua
@@ -1,9 +1,30 @@
+local frame = CreateFrame("FRAME", "SodaScrapFrame", ScrappingMachineFrame);
+frame:RegisterEvent("SCRAPPING_MACHINE_SHOW");
+frame:RegisterEvent("SCRAPPING_MACHINE_CLOSE");
+
+open = false;
+local function eventHandler(self, event, ...)
+  if (event == "SCRAPPING_MACHINE_SHOW") then
+    print ("open")
+    open = true;
+    checkItems();
+  else
+    print ("closed")
+    open = false;
+  end
+end
+
+
+frame:SetScript("OnEvent", eventHandler);
+
 SLASH_SCRAPPER1, SLASH_SCRAPPER2 = "/scrapper", "/scrap";
 
 addedItems = 0;
 
 function SlashCmdList.SCRAPPER(msg, editbox)
+  if (open == true) then
   checkItems(self);
+  end 
 end 
 
 function checkItems(self)
